67101194e2b5a5b2f3d7cd6d88e7e1fb11702c8e
pakayb <pakayb@gmail.com>
2019-11-28 12:31:22 +0100

    Add project files.

---
+++ SerializePeople.Tests/PersonTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using SerializePeople;
+
+namespace SerializePeople.Tests
+{
+    [TestFixture]
+    public class PersonTest
+    {
+
+        [Test]
+        public void PersonConstructor_WithValidData_CreatePersonObject()
+        {
+            var birthDate = new DateTime(1999, 11, 10);
+            var person = new Person("John", birthDate, Person.Genders.Male);
+            Assert.AreEqual("John",person.Name);
+            Assert.AreEqual(birthDate,person.BirthDate);
+            Assert.AreEqual(Person.Genders.Male,person.Gender);
+            Assert.AreEqual(20,person.Age);
+            
+        }
+
+        [Test]
+        public void PersonConstructor_BirthDayIsAfterThisMounthButInThisYear_GiveTheCorrectAge()
+        {
+            var testDateTime = DateTime.Now;
+            testDateTime = testDateTime.AddYears(-30);
+            if (testDateTime.Month<12)
+            {
+                testDateTime = testDateTime.AddMonths(1);
+            }
+            var person = new Person("John", testDateTime,Person.Genders.Male);
+            Assert.AreEqual(29,person.Age);
+        }
+
+        [Test]
+        public void PersonConstructor_BirthDayIsTomorrow_GiveTheCorrectAge()
+        {
+            var testDateTime = DateTime.Now;
+            testDateTime = testDateTime.AddYears(-20);
+            testDateTime = testDateTime.AddDays(1);
+            var person = new Person("John",testDateTime,Person.Genders.Male);
+            Assert.AreEqual(19,person.Age);
+        }
+
+        [Test]
+        public void PersonConstructor_BirthDayIsToday_GiveTheCorrectAge()
+        {
+            var testDate = DateTime.Now;
+            testDate = testDate.AddYears(-23);
+            var person = new Person("Barna",testDate,Person.Genders.Male);
+            Assert.AreEqual(23,person.Age);
+        }
+
+        [Test]
+        public void PersonTostring_ValidData_GiveTheCorrectStringBack()
+        {
+            var person = new Person("John", new DateTime(2000,01,01),Person.Genders.Male );
+            var expectedString = "Name: John | Gender: Male | Age: 19";
+            Assert.AreEqual(expectedString, person.ToString());
+        }
+    }
+}
---
+++ SerializePeople/Person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace SerializePeople
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; }
+        public DateTime BirthDate { get; }
+        public Genders Gender { get; }
+        public int Age { get; private set; }
+
+        public enum Genders
+        {
+            Male,
+            Female
+        }
+
+        public Person(string name, DateTime birthDate, Genders gender)
+        {
+            Name = name;
+            BirthDate = birthDate;
+            Gender = gender;
+            Age = CalculateAge(birthDate);
+        }
+
+        private int CalculateAge(DateTime birthDate)
+        {
+            var now = DateTime.Now;
+            var age = (now.Year - birthDate.Year);
+            if (birthDate.Month > now.Month || birthDate.Month == now.Month && birthDate.Day > now.Day )
+            {
+                age--;
+            }
+
+            return age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} | Gender: {Gender} | Age: {Age}";
+        }
+    }
+}
